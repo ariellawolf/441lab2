150c57e1587bdf8b98b1b15a96982a966b0877a8
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-10-01 18:17:39 +0000

    blink for lab

--- blink.py
+++ blink.py
@@ -2,32 +2,56 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-#pin 25 will be blinking
-p= 24
-#f= 1
-#dc= 50
-GPIO.setup(p, GPIO.OUT)
-print('code is running')
+#frequency for PWM
+f= 1
+#pins 23,24,25 are connected to LEDs
+LED1 = 23
+LED2 = 16
+LED3 = 25
 
-while True:
-  print('the light is flashing')
-  GPIO.output(p,0)
-  sleep(0.5)
-  GPIO.output(p,1)
-  sleep(0.5)
+#sets up LED pins as outputs
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+GPIO.setup(LED3, GPIO.OUT)
 
-GPIO.cleanup()
-'''
-#pwm = GPIO.PWM(print)
+#sets up button pins as pull-down inputs
+BUTTON1 = 5
+BUTTON2 = 6
+GPIO.setup(BUTTON1,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON2,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+print('All set up')
+
+#defining callback function
+def myCallback(turnOnLED):
+  if GPIO.input(BUTTON1)==1:
+    turnOnLED = LED1
+  elif GPIO.input(BUTTON2)==1:
+    turnOnLED = LED2
+  GPIO.PWM(turnOnLED, f)
+  pwm.start(dc)
+  #creating triangular waveform of 1 Hz
+  while 1:
+    for dc in range(101):
+      pwm.ChangeDutyCycle(dc)
+      sleep (0.005)
+    for dc in range(101)
+      pwm.ChangeDutyCycle(100-dc)
+      sleep(0.005)
+
 
 try:
-  pwm.start(dc)
   while True:
-    pass
+    print('the light is flashing')
+    gpio.add_event_detect(BUTTON1, gpio.BOTH, callback= myCallback, bouncetime=100)
+    gpio.add_event_detect(BUTTON2, gpio.BOTH, callback= myCallback,bouncetime=100)
+    #LED3 is blinking constantly at 1Hz
+    GPIO.output(LED3,0)
+    sleep(0.5)
+    GPIO.output(LED3,1)
+    sleep(0.5)
 except KeyboardInterrupt:
   print('\nExiting')
+except Exception as e:
+  print('\ne')
 
-
-pwm.stop()
 GPIO.cleanup()
-'''
